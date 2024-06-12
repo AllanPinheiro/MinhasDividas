9bde62fa449bc53ac24dc49902bc18e6da55380d
pinheirogabrieldev@gmail.com <pinheirogabrieldev@gmail.com>
2024-06-11 21:37:10 -0300

    Adicionado soma de valor e imprimindo item na tela

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.SqlClient;
-using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
 
 namespace MinhasDividas
 {
@@ -8,7 +9,9 @@ namespace MinhasDividas
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
         // BOTAO ADICIONAR
         private void btnAdd_Click(object sender, EventArgs e)
         {
@@ -18,26 +21,23 @@ namespace MinhasDividas
             if (string.IsNullOrEmpty(desc) && string.IsNullOrEmpty(valor))
             {
                 lblStatus.Text = "Por favor, preencha os campos.";
+                return;
             }
             else if (string.IsNullOrEmpty(desc) || string.IsNullOrEmpty(valor))
             {
                 lblStatus.Text = "Por favor, preencha todos os campos.";
+                return;
             }
-            else
-            {
-                lblStatus.ResetText();
-                txtDesc.ResetText();
-                txtValor.ResetText();
-            }
+
             // CONEXAO COM BANCO DE DADOS
-            /*string connectionString = "";
+            string connectionString = "Data Source=DESKTOP-QRDBJEB\\SQLEXPRESS;Initial Catalog=DBMINHASDIVIDA;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
                     connection.Open();
                     // METODO INSERT INTO NO BANCO DE DADOS
-                    string query = "INSERT INTO DIVIDA (DESCRICAO, VALOR) VALUES (@DESCRICAO, @VALOR)";
+                    string query = "INSERT INTO DIVIDAS (DESCRICAO, VALOR) VALUES (@DESCRICAO, @VALOR)";
                     SqlCommand cmd = new SqlCommand(query, connection);
                     // EVITAR SQL INJECTION
                     cmd.Parameters.AddWithValue("@DESCRICAO", desc);
@@ -48,27 +48,59 @@ namespace MinhasDividas
                         lblStatus.Text = "Adicionado com sucesso!";
                         txtDesc.ResetText();
                         txtValor.ResetText();
-                    }
-                    else
-                    {
-                        lblStatus.Text = "Erro ao adicionar";
-                        txtDesc.ResetText();
-                        txtValor.ResetText();
+                        LoadData();
                     }
                 }
                 catch (Exception ex)
                 {
-                    lblStatus.Text = ex.Message;
+                    MessageBox.Show("Erro ao carregar os dados: " + ex.Message);
                 }
-            }*/
+            }
         }
+
+        // LOAD DE ITEM ADICIONADO
+        private void LoadData()
+        {
+            string connectionString = "Data Source=DESKTOP-QRDBJEB\\SQLEXPRESS;Initial Catalog=DBMINHASDIVIDA;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT DESCRICAO, VALOR FROM DIVIDAS ORDER BY VALOR ASC";
+                    SqlDataAdapter da = new SqlDataAdapter(query, connection);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    lstItems.Items.Clear();
+                    decimal somaTotal = 0; // Variável para acumular a soma dos valores
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        decimal valor = Convert.ToDecimal(row["VALOR"]);
+                        somaTotal += valor; // Adiciona o valor atual à soma total
+                        string valorFormatado = valor.ToString("C2", new System.Globalization.CultureInfo("pt-BR"));
+                        lstItems.Items.Add($"----- Descrição: {row["DESCRICAO"]} ----- Valor R$: {valorFormatado} -----");
+                    }
+                    // Formatar e exibir a soma total
+                    string somaTotalFormatada = somaTotal.ToString("C2", new System.Globalization.CultureInfo("pt-BR"));
+                    lblSomaValor.Text = $"{somaTotalFormatada}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao carregar os dados: " + ex.Message);
+                }
+            }
+        }
+
         // BOTAO DELETAR
         private void btnDel_Click(object sender, EventArgs e)
         {
+            // VERIFICA CAMPO SE ESTAO VAZIO
             string descDel = txtDel.Text;
             if (string.IsNullOrEmpty(descDel))
             {
                 lblStatusDel.Text = "Preencha o campo deletar.";
+                return;
             }
             else
             {
@@ -76,18 +108,22 @@ namespace MinhasDividas
                 txtDel.ResetText();
             }
         }
+
         // BOTAO EDITAR
         private void btnEditar_Click(object sender, EventArgs e)
         {
+            // VERIFICA CAMPOS SE ESTAO VAZIO
             string descEdit = txtDescEdit.Text;
             string valorEdit = txtValorEdit.Text;
             if (string.IsNullOrEmpty(descEdit) && string.IsNullOrEmpty(valorEdit))
             {
                 lblStatusEditar.Text = "Preencha os campos editar.";
+                return;
             }
             else if (string.IsNullOrEmpty(descEdit) || string.IsNullOrEmpty(valorEdit))
             {
                 lblStatusEditar.Text = "Preencha todos os campos editar.";
+                return;
             }
             else
             {
